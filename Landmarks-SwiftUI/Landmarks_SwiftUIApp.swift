a1ccec27417b0768f51617cfb94a2051ea3cd405
valenyang <valenyala@gmail.com>
2024-11-15 22:00:15 -0300

    Initial Commit

---
+++ Landmarks-SwiftUI/Landmarks_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks_SwiftUIApp.swift
+//  Landmarks-SwiftUI
+//
+//  Created by Valentin Yang on 15/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
